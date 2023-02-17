e8a7829067851a846167fbdff7b45a47e1146d9f
Diogo Borges <54003586+diogo7b@users.noreply.github.com>
2023-02-17 10:14:40 -0300

    solved: error when throw an exception in UserRegisterUseCase

--- __tests__/user-register-use-case.spec.ts
+++ __tests__/user-register-use-case.spec.ts
@@ -1,7 +1,7 @@
 class UserResgisterUseCase {
     constructor() { }
 
-    async save(name: string, email: string, password: string): Promise<string> {
+    async save(name: string, email: string, password: string): Promise<String> {
         if (!email) {
             throw new Error("Missing Params")
         }
@@ -23,12 +23,12 @@ const userDataMissingParam = {
 describe("User register use cases", () => {
     it("Should return success with correct params", async () => {
         const userResgisterUseCase = new UserResgisterUseCase()
-        const response = await userResgisterUseCase.save(userData.name, userData.email, userData.password)
-        await expect(response).toBe("User registered")
+        const promise = await userResgisterUseCase.save(userData.name, userData.email, userData.password)
+        await expect(promise).toBe("User registered")
     }),
         it("Should throw an error if missing params", async () => {
             const userResgisterUseCase = new UserResgisterUseCase()
-            const response = await userResgisterUseCase.save(userDataMissingParam.name, userDataMissingParam.email, userDataMissingParam.password)
-            await expect(response).rejects.toThrow(new Error("Missing Params"))
+            const promise = userResgisterUseCase.save(userDataMissingParam.name, userDataMissingParam.email, userDataMissingParam.password)
+            await expect(promise).rejects.toThrow()
         })
 })
